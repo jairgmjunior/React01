3b33d0a1e70734455420f9d721e8861ccf76469b
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-09-04 05:03:18 -0300

    propriedades jsx

--- src/components/PrimeiroComponent.jsx
+++ src/components/PrimeiroComponent.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-export default () => (
+let isLegal = false;
+
+export default (props) => (
     <div>
-        <h1>Primeiro Componente (arrow)</h1>
+        <h1>{props.valor}</h1>
+        <h1>{props.elevado}</h1>
+        <h1>{5*5}</h1>
+        <h1>legal? {isLegal ? 'Sim' : 'Não'}</h1>
+        <h1>{Math.random(5,6)}</h1>
     </div>
 )
+
+// export default () => (
+//     <div>
+//         <h1>Primeiro Componente (arrow)</h1>
+//     </div>
+// )
 
 /*
 function primeiro(){
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import PrimeiroComponente from './components/PrimeiroComponent'
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <PrimeiroComponente />
+        <PrimeiroComponente valor="Bom dia" elevado={Math.pow(2,8)}/>
     </div>
     
     , elemento)
