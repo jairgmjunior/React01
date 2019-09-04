643fa526908370edcb1b5d2d950817015b3278fa
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-09-04 05:41:19 -0300

    Relação entre componentes #2

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './components/PrimeiroComponent'
 // import {CompA, CompB as ComponentB} from './components/MultiplesComponents'
 //import MultElements from './components/MultiplesElements'
-import Familia from './components/FamiliaSilva'
+//import Familia from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Membro from './components/Member'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <Familia/>
+        <Familia>
+            <Membro nome="José" sobrenome="Antonio"/>
+            <Membro nome="João" sobrenome="Antonio"/>
+        </Familia>
+        <Familia>
+            <Membro nome="Maria" sobrenome="Raimunda"/>
+            <Membro nome="Daniela" sobrenome="Maria"/>
+        </Familia>
     </div>    
     , elemento)
 
+// const elemento = document.getElementById('root')
+// ReactDOM.render(
+//     <div>
+//         <Familia/>
+//     </div>    
+//     , elemento)
 
     // const elemento = document.getElementById('root')
 // ReactDOM.render(
