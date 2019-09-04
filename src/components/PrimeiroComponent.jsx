9debb7c114d84a691aae78b9abaaf661dcfad628
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-09-04 04:51:16 -0300

    primeiro component com arrow function

--- src/components/PrimeiroComponent.jsx
+++ src/components/PrimeiroComponent.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
+export default () => (
+    <div>
+        <h1>Primeiro Componente (arrow)</h1>
+    </div>
+)
+
+/*
 function primeiro(){
     return <h1>Primeiro Componente!</h1>
 }
 
 export default primeiro;
+
+
+*/
